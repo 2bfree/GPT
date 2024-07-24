45ad459674b823128fca8c3eec7c96d6f025367a
KCCGLASS-01 <jmha0140@homecc.com>
2024-07-24 11:32:57 +0900

    Update kcc.py

--- kcc.py
+++ kcc.py
@@ -20,7 +20,7 @@ import streamlit as st
 if "messages" not in st.session_state:
     st.session_state["messages"]=[{"role":"assistant","content":"안녕하세요, KCC글라스 사내규정 챗봇입니다. 무엇을 도와드릴까요?"}]
     st.caption("Groupware > 사내정보 > 기타정보 > KCC 사규/매뉴얼 관리시스템에서도 확인 할 수 있습니다.")
-    st.info("  ※ 간결하지만 구체적으로 질문해주세요.   ex) 출장규정 알려줘 > 국내 출장 여비 알려줘" , icon="ℹ️ )
+    st.info("  ※ 간결하지만 구체적으로 질문해주세요.   ex) 출장규정 알려줘 > 국내 출장 여비 알려줘" , icon="ℹ️" )
     st.info("사용자가 많을 경우 답변이 느려질 수 있습니다.", icon="ℹ️")
     # :grey_exclamation: 사용자가 많을 경우 답변이 느려질 수 있습니다.")
     # st.text("  ※ 사내규정 URL >> http://manual.kccworld.info/kccrules2/usermaintype.do")
