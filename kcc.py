b7de96d05e68e49358900e5cf2e3ec8eb8b43509
KCCGLASS-01 <jmha0140@homecc.com>
2024-07-26 09:22:39 +0900

    Update kcc.py

--- kcc.py
+++ kcc.py
@@ -1,32 +1,29 @@
 import time
 from openai import OpenAI
 import streamlit as st
-
+import os
 
 assistant_id = 'asst_gSo5oyon5bH785Wcw59V2obe'
 openai_api_key = st.secrets['OPENAPI_KEY']
 client = OpenAI(api_key=openai_api_key)
+thread_id = 'thread_dZa3CREaD7kBBtlgZIoALyYb'
+vector_id ='vs_syyWW8UtZcNVf54nyrQxp40G'
 
-st.title("KCC글라스 사내규정 챗봇")
-# st.caption("LLM-GPT-3.5-TURBO")
+assistant = client.beta.assistants.update(
+  assistant_id=assistant_id,
+  tool_resources={"file_search": {"vector_store_ids": [vector_id]}},
+)
 
-thread = client.beta.threads.create()
-thread_id = thread.id
-
-import streamlit as st
-
-# st.info('This is a purely informational message', icon="ℹ️")
 
 if "messages" not in st.session_state:
     st.session_state["messages"]=[{"role":"assistant","content":"안녕하세요, KCC글라스 사내규정 챗봇입니다. 무엇을 도와드릴까요?"}]
     st.caption("Groupware > 사내정보 > 기타정보 > KCC 사규/매뉴얼 관리시스템에서도 확인 할 수 있습니다.")
     st.info(" 간결하지만 구체적으로 질문해주세요.  :pushpin: 출장규정 알려줘 > 국내 출장 여비 알려줘" , icon="ℹ️" )
     st.info(" 사용자가 많을 경우 답변이 느려질 수 있습니다.", icon="ℹ️")
-    # :grey_exclamation: 사용자가 많을 경우 답변이 느려질 수 있습니다.")
-    # st.text("  ※ 사내규정 URL >> http://manual.kccworld.info/kccrules2/usermaintype.do")
-   # :heavy_check_mark:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
+
+
 
 if prompt := st.chat_input():
     if not openai_api_key:
@@ -40,41 +37,31 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role":"user","content":prompt})
     st.chat_message("user").write(prompt)
 
-    response = client.beta.threads.messages.create(
-        thread_id,
-        role="user",
-        content=prompt+" 한국어로 말해줘",
-    )
-    print(response)
 
-    run = client.beta.threads.runs.create(
-        thread_id=thread_id,
-        assistant_id=assistant_id
-    )
-    run_id = run.id
 
+    message = client.beta.threads.messages.create(
+        thread_id = thread_id,
+        role = "user",
+        content= prompt
+    )
     while True:
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread_id,
-            run_id=run_id
+        run = client.beta.threads.runs.create_and_poll(
+            thread_id = thread_id,
+            assistant_id = assistant.id,
         )
 
-        if run.status == "completed":
+        if run.status == 'completed':
             break
         else:
-            time.sleep(20)
             break
-        print(run)
+            print(run.status)
+
+
 
     thread_messsage = client.beta.threads.messages.list(thread_id)
     msg = thread_messsage.data[0].content[0].text.value
-    time.sleep(10)
+    time.sleep(1)
     st.session_state.messages.append({"role": "assisant", "content": msg})
     st.chat_message("assistant").write(msg)
 
 
-
-    # response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    # msg = response.choices[0].message.content
-    # st.session_state.messages.append({"role":"user","content":msg})
-    # st.chat_message("assistant").write(msg)
