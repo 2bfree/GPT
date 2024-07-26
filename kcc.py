d01e1a7dda81d56623a6e0e2a4a98e7ec27109ca
KCCGLASS-01 <jmha0140@homecc.com>
2024-07-26 09:32:25 +0900

    Update kcc.py

--- kcc.py
+++ kcc.py
@@ -3,11 +3,11 @@ from openai import OpenAI
 import streamlit as st
 import os
 
-assistant_id = st.secrets['ASSISTANT_ID']
+assistant_id = 'asst_gSo5oyon5bH785Wcw59V2obe'
 openai_api_key = st.secrets['OPENAPI_KEY']
 client = OpenAI(api_key=openai_api_key)
-thread_id =st.secrets['THREAD_ID ']
-vector_id =st.secrets['VECTOR_ID']
+thread_id = 'thread_dZa3CREaD7kBBtlgZIoALyYb'
+vector_id ='vs_syyWW8UtZcNVf54nyrQxp40G'
 
 assistant = client.beta.assistants.update(
   assistant_id=assistant_id,
