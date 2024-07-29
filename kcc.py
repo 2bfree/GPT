1df29f8c67c9611a332bea33e6df5d4b6ae49f19
KCCGLASS-01 <jmha0140@homecc.com>
2024-07-29 17:09:14 +0900

    Update kcc.py

--- kcc.py
+++ kcc.py
@@ -3,37 +3,117 @@ from openai import OpenAI
 import streamlit as st
 import os
 
+
 assistant_id = 'asst_gSo5oyon5bH785Wcw59V2obe'
 openai_api_key = st.secrets['OPENAPI_KEY']
 client = OpenAI(api_key=openai_api_key)
-# thread_id = 'thread_dZa3CREaD7kBBtlgZIoALyYb'
+
 thread = client.beta.threads.create()
 thread_id = thread.id
 st.title("KCC글라스 사내규정 챗봇")
 
-vector_id ='vs_syyWW8UtZcNVf54nyrQxp40G'
+vector_id = 'vs_syyWW8UtZcNVf54nyrQxp40G'
 
 assistant = client.beta.assistants.update(
-  assistant_id=assistant_id,
-  tool_resources={"file_search": {"vector_store_ids": [vector_id]}},
+    assistant_id=assistant_id,
+    tool_resources={"file_search": {"vector_store_ids": [vector_id]}},
 )
+
+st.sidebar.title('규정 카테고리')
+st.sidebar.header('[전산]')
+st.sidebar.text(' - 전산관리규정')
+st.sidebar.text(' - 업무전산화규칙')
+st.sidebar.text(' - 백업관리규칙')
+st.sidebar.text(' - 데이터베이스 및 프로그램관리')
+st.sidebar.text(' - 전산기기관리규칙')
+st.sidebar.text(' - 소프트웨어관리규칙')
+st.sidebar.text(' ')
+
+st.sidebar.header('[인사]')
+st.sidebar.text(' - 취업규칙')
+st.sidebar.text(' - 촉탁사원관리')
+st.sidebar.text(' - 근태관리시행')
+st.sidebar.text(' - 해외주재원처우')
+st.sidebar.text(' - 연수규정')
+st.sidebar.text(' - 자기계발지원')
+st.sidebar.text(' - 징계/포상')
+st.sidebar.text(' - 신원재정보증업무')
+st.sidebar.text(' - 관계사간 전출입지급여 등 지급규칙')
+st.sidebar.text(' - 하자및폐기발생에 대한 사후조치')
+st.sidebar.text(' - 회수불능채권에 대한 사후조치')
+st.sidebar.text(' ')
+
+
+st.sidebar.header('[경영]')
+st.sidebar.text(' - 국내/해외출장')
+st.sidebar.text(' - 복리후생')
+st.sidebar.text(' - 문서관리/보관')
+st.sidebar.text(' - 인증서관리')
+st.sidebar.text(' - 동호회지원')
+st.sidebar.text(' - 경조금지급')
+st.sidebar.text(' - 자가운전보조금 등 ')
+st.sidebar.text(' - 개인휴대전화지원')
+st.sidebar.text(' - 장학급지급')
+st.sidebar.text(' - 주택자금대부')
+st.sidebar.text(' - 생활안전자금대부')
+st.sidebar.text(' - 의료비지원')
+st.sidebar.text(' - 질병위로금')
+st.sidebar.text(' - 노하우자료관리')
+st.sidebar.text(' - 전자문서결재')
+st.sidebar.text(' - 업무매뉴얼관리')
+st.sidebar.text(' ')
+
+st.sidebar.header('[안전]')
+st.sidebar.text(' - 안전보건관리')
+st.sidebar.text(' - 비상시업무')
+st.sidebar.text(' - 보안')
+st.sidebar.text(' - 당직근무')
+st.sidebar.text(' - 안전사고발생에 대한 사후조치')
+st.sidebar.text(' ')
+
+
+st.sidebar.header('[자산]')
+st.sidebar.text(' - 부동산관리')
+st.sidebar.text(' - 차량관리')
+st.sidebar.text(' - 자산처분심의')
+st.sidebar.text(' - 사택관리')
+st.sidebar.text(' - 교통법규위반 및 차량사고처리')
+st.sidebar.text(' ')
+
+st.sidebar.header('[대외일반]')
+st.sidebar.text(' - 준법규정')
+st.sidebar.text(' - 법률자문 및 송무처리규정')
+st.sidebar.text(' - 준법규칙')
+st.sidebar.text(' - 내부신고제도운영규칙')
+st.sidebar.text(' - 내부감사규정')
+st.sidebar.text(' ')
+
+st.sidebar.header('[거버넌스]')
+st.sidebar.text(' - 감사위원회')
+st.sidebar.text(' - 이사회운영')
+st.sidebar.text(' - 사외이사후보추천위원회')
+st.sidebar.text(' - 내부거래위원회')
+st.sidebar.text(' - 내부ESG위원회')
+st.sidebar.text(' - 보상위원회')
+
+st.sidebar.subheader('')
+st.sidebar.header('[정보보호]')
+
 
 
 if "messages" not in st.session_state:
-    st.session_state["messages"]=[{"role":"assistant","content":"안녕하세요, 사내 규정에 대해 질문해주세요."}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "안녕하세요, 사내 규정에 대해 질문해주세요."}]
     st.caption("Groupware > 사내정보 > 기타정보 > KCC 사규/매뉴얼 관리시스템에서도 확인 할 수 있습니다.")
 
-    st.success("규정 카테고리 : [전산] [인사] [경영] [안전] [정보보호] [자산] [준법] [거버넌스] ", icon="✔️")
-    st.success("복리 후생이 궁금하신 경우 : 『복리후생 관련 규정』 라고 질문해보세요.", icon="✔️")
+    # st.success("규정 카테고리 : [전산] [인사] [경영] [안전] [정보보호] [자산] [준법] [거버넌스] ", icon="✔️")
+    st.success(" ex) 사원일 때 휴대전화 지원금액 알려줘.", icon="✔️")
 
-#    st.success("규정 카테고리 : [인력운영]  [인력개발]  [인사행정]  [전산]  [경영일반]  [정보보호]  [거버넌스]", icon="✔️")
-#    st.warning(" 사용자가 많을 경우 답변이 느려질 수 있습니다.", icon="⚠️")
+    #    st.success("규정 카테고리 : [인력운영]  [인력개발]  [인사행정]  [전산]  [경영일반]  [정보보호]  [거버넌스]", icon="✔️")
+    #    st.warning(" 사용자가 많을 경우 답변이 느려질 수 있습니다.", icon="⚠️")
     st.caption("  ⚠️사용자가 많을 경우 답변이 느려질 수 있습니다.")
 st.divider()
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
-
-
 
 if prompt := st.chat_input():
     if not openai_api_key:
@@ -44,20 +124,18 @@ if prompt := st.chat_input():
         st.info("Check your Thread ID")
         st.stop()
 
-    st.session_state.messages.append({"role":"user","content":prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-
-
     message = client.beta.threads.messages.create(
-        thread_id = thread_id,
-        role = "user",
-        content= prompt
+        thread_id=thread_id,
+        role="user",
+        content=prompt
     )
     while True:
         run = client.beta.threads.runs.create_and_poll(
-            thread_id = thread_id,
-            assistant_id = assistant.id,
+            thread_id=thread_id,
+            assistant_id=assistant.id,
         )
 
         thread_messsage = client.beta.threads.messages.list(thread_id)
@@ -65,7 +143,6 @@ if prompt := st.chat_input():
         time.sleep(1)
         st.session_state.messages.append({"role": "assisant", "content": msg})
 
-      
         if run.status == 'completed':
             st.chat_message("assistant").write(msg)
             break
@@ -73,4 +150,3 @@ if prompt := st.chat_input():
             st.chat_message("assistant").write("규정을 찾을 수 없습니다.")
             break
             print(run.status)
-
